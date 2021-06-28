25813482f8eb4b27364f90ca3d4c35e884130641
Umut Turan <umutturan107@gmail.com>
2021-06-28 20:33:03 +0300

    Update Work 8 - Node.js & NPM

--- Work8-Node.js/intro-to-node/index.js
+++ Work8-Node.js/intro-to-node/index.js
@@ -1,19 +1,25 @@
+//jshint esversion:6
+
+
 
 // Native Node Module
+
 // const fs = require("fs");
 // fs.copyFileSync("file1.txt", "file2.txt");
 
 
+
 // Installing External Node Module
+
 // const superheroes = require('superheroes');
-//
 // var superHeroName = superheroes.random();
-//
 // // console.log(superHeroName);
 
 
-//                           CHALLANGE
+
+// CHALLANGE
 // Installing External Node Module: supervillains
+
 const supervillains = require('supervillains');
 var superVillianName = supervillains.random()
 console.log(superVillianName);
